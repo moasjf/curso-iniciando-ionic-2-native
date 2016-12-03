39792faa0f164d54853c5f9ac26cb1c20e0e1bd0
Moacir Jacomin <Moacir Jacomin>
2016-12-02 22:56:43 -0200

    NetworkMelhorado

--- app/pages/network-test/network-test.ts
+++ app/pages/network-test/network-test.ts
@@ -8,14 +8,33 @@ import { Network, Connection } from 'ionic-native';
 export class NetworkTestPage {
   connectionType: any;
 
-  constructor(private nav: NavController) {}
+  constructor(private nav: NavController) {
+
+    let connectSubscription = Network.onConnect().subscribe(() => {
+      console.log('network connected!'); 
+      // We just got a connection but we need to wait briefly
+      // before we determine the connection type.  Might need to wait 
+      // prior to doing any api requests as well.
+      setTimeout(() => {
+        if (Network.connection === 'wifi') {
+          console.log('we got a wifi connection, woohoo!');
+        }
+      }, 3000);
+    });
+
+  }
 
   check(){
     this.connectionType = Network.connection;
-    console.log('type:' + Network.connection);
+
+    console.log('Está no '+Network.connection);
 
     if(Network.connection == Connection.WIFI){
        console.log('Está no WIFI');
+    }
+
+    if(Network.connection == Connection.NONE){
+       console.log('Está no SEM coneccao');
     }
   }
 }
